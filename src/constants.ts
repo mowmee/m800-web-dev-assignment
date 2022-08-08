3573620bf14956d2c5e0b60377d1b4a9b9d7fe00
Mike <mike.tsaichan.hsieh@gmail.com>
2022-08-08 22:06:59 +0800

    build: mock api for development

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,10 @@
  *
  * https://openweathermap.org/forecast5
  */
-export const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+export const forecastUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.openweathermap.org/data/2.5/forecast'
+    : 'https://run.mocky.io/v3/1dfa6a0d-11f4-41ea-a23d-6ecf53249459';
 
 /**
  * required params: `q`, `appid`
@@ -14,6 +17,10 @@ export const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
  *
  * https://openweathermap.org/api/geocoding-api
  */
-export const geoUrl = 'http://api.openweathermap.org/geo/1.0/direct';
+export const geoUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://api.openweathermap.org/geo/1.0/direct'
+    : 'https://run.mocky.io/v3/e4e8bcbb-df91-4c69-8def-b7e7682d0f01';
 
-export const appid = process.env.REACT_APP_API_KEY;
+export const appid =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_KEY : 'key';
