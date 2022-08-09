97156e86ea087564b892154046dd97cd96c5fe8e
Mike <mike.tsaichan.hsieh@gmail.com>
2022-08-09 18:37:25 +0800

    feat: index layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import CountryInput from './components/CountryInput';
 import WeatherForecast from './components/WeatherForecast';
 import { useGeoData, useForecastData } from './hooks';
+import styled from 'styled-components';
 
 export default function App() {
   const [q, setQ] = useState('');
 
-  const { data: geoData } = useGeoData(q);
-  const { data: forecastData } = useForecastData(geoData?.[0]);
+  const { data: geoData, isValidating: isGeoLoading } = useGeoData(q);
+  const selectedGeo = geoData?.[0];
+
+  const { data: forecastData, isValidating: isForecastLoading } =
+    useForecastData(selectedGeo);
+
+  const resultCountryName = useMemo(() => {
+    switch (true) {
+      case !!selectedGeo:
+        return selectedGeo!.name;
+      case isGeoLoading || isForecastLoading:
+        return 'loading';
+      case !!q:
+        return 'No found';
+      default:
+        return '';
+    }
+  }, [selectedGeo, isGeoLoading, isForecastLoading, q]);
 
   return (
-    <div>
+    <S.Container>
+      <S.Title>{'M800 Web Developer - Assignment'}</S.Title>
       <CountryInput value={q} onValueChanged={setQ} />
+      <S.Description>{resultCountryName}</S.Description>
       <WeatherForecast data={forecastData} />
-    </div>
+    </S.Container>
   );
 }
+
+const S = {
+  Container: styled.div`
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  `,
+  Title: styled.h1`
+    margin: auto;
+  `,
+  Description: styled.div`
+    margin: auto;
+    font-size: 16px;
+  `,
+};
